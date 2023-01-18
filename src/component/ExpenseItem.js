2f83d4ac03e92620ec2e3de28ce5c44357217b99
mishraom12345 <mishrahariom525@gmail.com>
2023-01-18 19:21:37 +0530

    adding expense list

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>petrtol 100</div>
+      <div>
+        <h2>movie 200</h2>
+      </div>
+      <div>fashion 300</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
