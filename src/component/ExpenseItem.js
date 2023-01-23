518a3358b59893fb9569766490475579cddea1c0
mishraom12345 <mishrahariom525@gmail.com>
2023-01-23 11:01:08 +0530

    using props concept redering diffrent items

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,11 +1,26 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  const expensedate = new Date();
+  const expenseTitle = "car insurance"
+  const expenseAmount = 294
+  const locationofExpenditure = "location-bombay";
+  const expenseDate = new Date()
   return (
-    <div>
-      <div>petrtol 100</div>
+    <div className="expense-item">
       <div>
-        <h2>movie 200</h2>
+        <h5>{props.date.toDateString()}-</h5>
       </div>
-      <div>fashion 300</div>
+      <div className="expense-item__description">
+        <div>
+          <h5> {props.title} </h5>
+        </div>
+      </div>
+      <div>
+        <h2>{locationofExpenditure}</h2>
+      </div>
+      <div className="expense-item__price">
+        <h2>{props.amount}</h2>
+      </div>
     </div>
   );
 }
