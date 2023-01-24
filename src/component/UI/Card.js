f44982e77eb9778656b2fb8d8b0b52c41876ec1c
mishraom12345 <mishrahariom525@gmail.com>
2023-01-24 08:26:50 +0530

    organise card into Ui folder

---
+++ src/component/UI/Card.js
@@ -0,0 +1,13 @@
+import './Card.css'
+function Card(props){
+    const classes = 'card ' + props.className
+    console.log(props.children)
+    return(
+        
+       <div className={classes}>{props.children} </div>
+       
+    )
+    
+}
+
+export default Card
