4946c54f2be222f970cb1b6a3ccf3505f8a34c8b
mishraom12345 <mishrahariom525@gmail.com>
2023-01-24 17:26:43 +0530

    adding amount updation button using state

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,13 +1,29 @@
+import React ,{useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetail from "./ExpenseDetail";
 import Card from "../UI/Card";
 
 const ExpenseItem=(props)=> {
+  const [title,SetTitle] = useState(props.title)
+
+  const [amount,setAmount] = useState(props.amount)
+
+  const updateAmountHandler = ()=>{
+    setAmount('100')
+  }
  
   const clickHandler = ()=>{
-    console.log('clicked!!!!!')
+    SetTitle('Updated')
+    console.log('title')
   }
+
+  
+  
+
+  
+
+  
 
 
   return (
@@ -15,10 +31,12 @@ const ExpenseItem=(props)=> {
       <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
-      <div className="expense-item__price">{props.amount}</div>
+      <div className="expense-item__price">{amount}</div>
       <button onClick={clickHandler}>Change Title</button>
+      <button onClick={updateAmountHandler}>expense </button>
+      
     </Card>
     
     
